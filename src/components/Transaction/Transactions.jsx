92c4658b14303b7a468121ceb035310d9fd771f4
anastasyah86 <nastasyah86@gmail.com>
2021-03-29 11:55:38 +0300

    transactions done

--- src/components/Transaction/Transactions.jsx
+++ src/components/Transaction/Transactions.jsx
@@ -1,27 +1,39 @@
 import styles from './Transaction.module.css';
+import propTypes from 'prop-types';
 
 const TransactionHistory = ({ data }) => (
   <div className={styles.transaction}>
-    <h2> Transactions </h2>
-    <table className={styles.transaction}>
+    <h2 className={styles.header}> Transactions </h2>
+    <table className={styles.table}>
       <thead>
-        <tr>
+        <tr className={styles.miniheader}>
           <th>Type</th>
           <th>Amount</th>
           <th>Currency</th>
         </tr>
       </thead>
-      {data.map(({ id, type, amount, currency }) => (
-        <tbody>
+      <tbody className={styles.tbody}>
+        {data.map(({ id, type, amount, currency }) => (
           <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+            <td className={styles.type}>{type}</td>
+            <td className={styles.amount}>{amount}</td>
+            <td className={styles.currency}>{currency}</td>
           </tr>
-        </tbody>
-      ))}
+        ))}
+      </tbody>
     </table>
   </div>
 );
+
+TransactionHistory.propTypes = {
+  data: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.string.isRequired,
+      type: propTypes.string.isRequired,
+      amount: propTypes.string.isRequired,
+      currency: propTypes.string.isRequired,
+    }),
+  ),
+};
 
 export default TransactionHistory;
